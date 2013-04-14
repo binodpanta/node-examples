9b7c7f11c785c51edb9ccef6b210808c1c1aa1ad
Binod Pant <binod80@gmail.com>
2013-04-14 17:02:06 -0400

    Add streams demo

---
+++ streams/index.js
@@ -0,0 +1,7 @@
+// Demo of how to use streams
+
+// 
+var echoStdinToAnotherTerm = require('echostdintofile');
+
+// this shows how to echo anything you type in the REPL to a file
+echoStdinToAnotherTerm.demo();
