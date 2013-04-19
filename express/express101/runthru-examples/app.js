593424cdbeb10e4fcf1243c17762cdac2317e482
OpenShift guest <binod.pant@gmail.com>
2013-04-18 23:20:49 -0400

    add a cookie parsing middleware

--- express/express101/runthru-examples/app.js
+++ express/express101/runthru-examples/app.js
@@ -12,16 +12,25 @@ var express = require('express')
 
 var app = express();
 
+// Just a quick trial of printing out cookies
+var cookielogger = function(req,res,next){
+    if (req.cookies) 
+        console.log('Cookies were sent:' + JSON.stringify(req.cookies));
+    next();
+};
+
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon(path.join(__dirname, "public", "images", "favicon.ico")));
   app.use(express.logger('dev'));
+  app.use(express.cookieParser());
+  app.use(cookielogger);
+  app.use(express.static(path.join(__dirname, 'public')));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
   // to get a directory listing? This must come after the router middleware
   app.use(express.directory(path.join(__dirname, 'public')));
 });
@@ -30,16 +39,16 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-  // try adding my own middleware that does error handling
-  // Adding this after the standard express errorHandler
-  app.use(function(err,req,res,next) {
-      if (err) {
-          res.status(500);
-      }
+// try adding my own middleware that does error handling
+// Adding this after the standard express errorHandler
+app.use(function(err,req,res,next) {
+      if(err)
+          res.send('An error occurred with Error code: ' + err.code + ', : message: ' + err.message);
   });
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+
 
 // Try a custom route
 // NOTE: HAD TO RESTART THE APP BEFORE THIS TOOK EFFECT!
