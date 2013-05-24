a95c54dcf41ca20969e0a8dc8908a9e88cdc9080
Binod Pant <binod80@gmail.com>
2013-05-23 21:27:44 -0400

    mongodb example first checkin

---
+++ mongodb/test.js
@@ -0,0 +1,54 @@
+var mongodb = require('mongodb');
+var fs = require('fs');
+var argv = require('optimist')
+    .usage('Usage: $0 -root [dir/loc] -find [regexp]')
+    .demand(['root','find'])
+    .argv;
+
+
+// given a comd line input
+// test.js rootlocation regexp
+// finds all files where regexp is found, and stores info about that
+// in a collection
+// for each file where the regexp is found
+// a document is added like this {location:string,occurrences:int}
+
+var root = argv.root;
+var find = argv.find;
+
+if (!fs.existsSync(root)) throw root + " does not exist";
+
+var server = new mongodb.Server('localhost',27017,{auto_reconnect:true});
+
+var db = new mongodb.Db('mydb', server);
+
+// if pat found in file, record number of entries and file path in database
+var insert = function(f, pat) {
+    console.log('searching for ' + pat + ' in ' + f);
+}
+
+db.open(function(err,db) {
+    if (err) throw(err);
+    db.collection('widgets', function(err, coll) {
+	if (err) throw (err);
+        // null means remove all collections
+	coll.remove(null, {safe:true}, function(err, res) {
+		console.log("Removed " + coll + " result = " + res);
+	});
+
+
+        // insert some documents
+
+        fs.readdir(root,function(err,files){
+        if(err) throw err;
+        files.forEach(function(file){
+             // insert info about file if found pattern
+             insert(file, find);
+        });
+        });
+ 
+    });
+});
+
+
+
