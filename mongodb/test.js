25a904f021bd03225fc1539957786a9a16d5f38f
Binod Pant <binod80@gmail.com>
2013-05-23 23:36:22 -0400

    Create a new collection for each search string

--- mongodb/test.js
+++ mongodb/test.js
@@ -1,8 +1,8 @@
 var mongodb = require('mongodb');
 var fs = require('fs');
 var argv = require('optimist')
-    .usage('Usage: $0 -root [dir/loc] -find [regexp]')
-    .demand(['root','find'])
+    .usage('Usage: $0 -root [dir/loc] -find [regexp] -filepat [pat]')
+    .demand(['root','find','filepat'])
     .argv;
 
 // given a comd line input
@@ -12,8 +12,9 @@ var argv = require('optimist')
 // for each file where the regexp is found
 // a document is added like this {location:string,occurrences:int}
 
-var root = argv.root;
-var find = argv.find;
+var root = argv.root; // root where to begin searching
+var find = argv.find; // string to search in the files
+var filepat = new RegExp(argv.filepat);// pattern for file names to include
 
 if (!fs.existsSync(root)) throw root + " does not exist";
 
@@ -31,20 +32,25 @@ var insert = function(coll, f, pat) {
 
 db.open(function(err,db) {
     if (err) throw(err);
-    db.collection('widgets', function(err, coll) {
+
+    // Create a collection with the name which is the same as the
+    // find string 'find'. So every new search string will create
+    // a new collection and its search / find history stored 
+    // in that collection
+    db.collection(find, function(err, coll) {
 	if (err) throw (err);
         // null means remove all collections
-	coll.remove(null, {safe:true}, function(err, res) {
-		console.log("Removed " + coll + " result = " + res);
-	});
+	//coll.remove(null, {safe:true}, function(err, res) {
+	//	console.log("Removed " + coll + " result = " + res);
+	//});
 
 
         // insert some documents
         var finder = require('findit').find(root);
         //This listens for files found
         finder.on('file', function (file) {
-               var dotstart = /^\..*/g;
-               if (dotstart.test(file)) return;
+               var dotstart = filepat; // /.*\.r000$/g;
+               if (!dotstart.test(file)) return;
                fs.readFile(file, 'utf8', function(err,data) {
                           if (data.search(find)>-1){
                                console.log('Found it in '+ file + '!');
